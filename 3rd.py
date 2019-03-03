ff56ba1a1091fa0c3d88bcc6e776abc6fef9f879
plickoff <seacabofeat@gmail.com>
2019-03-03 22:01:07 +0800

    update

--- 3rd.py
+++ 3rd.py
@@ -1,3 +1,10 @@
-import ooptparse
+import optparse
 parser=optparse.OptionParser('usage %prog -H'+'<target host> -p <target port>')
 parser.add_option('-R',dest='tgtHost',type='string',help='specify target host')
+parser.add_option('-p',dest='tgtPort',type='int',help='specify target port')
+(options,args) =parser.parse_args()
+tgtHost = options.tgtHost
+tgtPort = options.tgtPort
+if (tgtHost == None) | (tgtPort == None):
+    print parser.usage
+    exit(0)
