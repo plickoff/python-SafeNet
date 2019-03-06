b9546515cb46a5c5878e72acaca1a8c0604a01f3
plickoff <seacabofeat@gmail.com>
2019-03-06 23:55:02 +0800

    update

--- 4th.py
+++ 4th.py
@@ -3,3 +3,6 @@ from socket import *
 def connScan (tgtHost,tgtPort):
     try:
         connSkt=socket(AF_INET,SOCK_STREAM)
+        connSkt=connect((tgtHost,tgtPort))
+        print '[+]%d/tcp open' % tgtPort
+        connSkt.close()
